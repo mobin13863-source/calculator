99dac73c25bb98fd0e25d8a83f30d29c0e3d29c3
Mobin <234991811+mobin13863-source@users.noreply.github.com>
2025-11-13 18:23:11 +0330

    fuuny game
    a write a fuuny game whit (and)  continye
    and some else

---
+++ hiphop_game.py
@@ -0,0 +1,28 @@
+n=0
+while n <20:
+    n+=1
+    if n%3==0 and n%5==0:
+        # this line of code it will never run because above=در بالا, both 5 and3 were printed
+        print("hiphoop")
+        continue
+
+    
+    if n %3==0:
+
+        print("hip")
+       
+        continue
+
+    if n%5==0:
+
+        print("hoop")
+       
+        continue
+    
+    
+
+    print(n)# this line of code it causes=باعث میشه number like 1,2 become print 
+
+
+
+
