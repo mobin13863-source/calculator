a7e15243bb5e3fddb9b2d694d747265a1043a130
Mobin <234991811+mobin13863-source@users.noreply.github.com>
2025-11-13 17:15:06 +0330

    Create GAME_5.py
    this is my fifeth game

---
+++ GAME_5.py
@@ -0,0 +1,13 @@
+print('hi')
+a=input('do you whant a play with a andome or whant you chose;')
+if a=='randome1':
+    print('ok leth go')
+    import random 
+    b=input('do you whant start')
+    if b=='yas':
+        print( random.randint(1, 6))
+    aa=random
+    if aa ==1:
+        print('you shoud go left','tipe left ')
+        left==aaa
+        
