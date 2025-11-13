4aa5c33df3b739bf41195fb12625ac1cf1717652
Mobin <234991811+mobin13863-source@users.noreply.github.com>
2025-11-13 16:23:42 +0330

    second game
    this is my second game

---
+++ game.py
@@ -0,0 +1,16 @@
+print('hi')
+a=input('do you want play a geme;')
+if a=='yas':
+    print('leth go')
+else:
+    print(exit())
+b=input('select vay;')
+if b=='ritht':
+     print('you di')
+elif b=='left':
+     print('ther was trap you di')
+elif b=='stay':
+    print('you win')
+else:
+    print('cant find it')
+ 
